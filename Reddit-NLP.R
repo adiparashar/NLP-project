dc42f904ec5176b0447145c6bbb9fce2b57af1c8
aditya parashar <adityaparashar95@gmail.com>
2016-12-23 13:36:03 +0530

    Update Reddit-NLP.R

--- Reddit-NLP.R
+++ Reddit-NLP.R
@@ -1,6 +1,4 @@
-# This R environment comes with all of CRAN preinstalled, as well as many other helpful packages
-# The environment is defined by the kaggle/rstats docker image: https://github.com/kaggle/docker-rstats
-# For example, here's several helpful packages to load in 
+
 
 library(ggplot2) # Data visualization
 library(readr) # CSV file I/O, e.g. the read_csv function
@@ -16,18 +14,16 @@ library(scales)
 library(lubridate)
 library(purrr)
 library(broom)
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-system("ls ../input")
 
-# Any results you write to the current directory are saved as output.
+
+
 require(RSQLite)
 require(dplyr)
 library(reshape2)
 dataframe<- function(subreddit){
            sub<- subreddit
-           db <- src_sqlite('../input/database.sqlite', create = F)
+           db <- src_sqlite('database.sqlite', create = F)
            db_subset <- db %>%
               tbl('May2015')
             
